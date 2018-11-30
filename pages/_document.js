d54bd6ed8698226f989e5fc3e4216fbe28c35b65
Stefan Kracht <stefankracht@gmail.com>
2018-11-30 15:04:06 +0100

    include progress css

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width" />
           <link rel="stylesheet" href="/static/styles.css" />
+          <link rel="stylesheet" href="/static/nprogress.css" />
         </Head>
         <body>
           <Main />
