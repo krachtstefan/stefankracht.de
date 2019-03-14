af9f0de5c1d53e6f1e4acd0abaab2bd4b86add6f
Stefan Kracht <stefankracht@gmail.com>
2019-03-14 16:03:28 +0100

    render markdown in index file

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,58 @@
-const postFileNames =
+const postsFolders =
   preval`
-module.exports = require("fs").readdirSync("./posts")
+module.exports = require("fs").readdirSync("./blog/")
 ` || [];
 
-import { Component } from 'react';
-import Layout from '../components/layout';
+const posts = postsFolders.map(folder => {
+  const {
+    default: Component,
+    meta: { title }
+  } = require(`../blog/${folder}/index.mdx`);
 
-export default class extends React.Component {
-  static async getInitialProps({ req }) {
-    const posts = postFileNames.map(name => {
-      //   const {
-      //     default: Component,
-      //     meta: { title }
-      //   } = require("../posts/" + name)
-      //   return {
-      //     Component,
-      //     title
-      //   }
-    });
-    return {};
-  }
+  return {
+    Component,
+    title
+  };
+});
 
-  render() {
-    return (
-      <Layout>
-        <h1>Hello</h1>
-      </Layout>
-    );
-  }
-}
+export default () => (
+  <div>
+    <h1>My Blog</h1>
+    {posts.map(post => (
+      <>
+        <h2>{post.title}</h2>
+        <post.Component />
+      </>
+    ))}
+  </div>
+);
+
+// import { Component } from 'react';
+// import Layout from '../components/layout';
+
+// export default class extends React.Component {
+//   static async getInitialProps({ req }) {
+//     const posts = postFileNames.map(name => {
+//       //   const {
+//       //     default: Component,
+//       //     meta: { title }
+//       //   } = require("../posts/" + name)
+//       //   return {
+//       //     Component,
+//       //     title
+//       //   }
+//     });
+//     return {};
+//   }
+
+//   render() {
+//     return (
+//       <Layout>
+//         <h1>Hello</h1>
+//       </Layout>
+//     );
+//   }
+// }
 
 //   render() {
 //     // console.log(posts)
