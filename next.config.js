e749f8b74545520bac50ba94d7cee0c5f53c499a
Stefan Kracht <stefankracht@gmail.com>
2019-03-15 13:27:36 +0100

    configure sass

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const withSass = require('@zeit/next-sass');
 const withMDX = require('@zeit/next-mdx')({
   extension: /\.mdx?$/
 });
 
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx']
-});
+module.exports = withSass(
+  withMDX({
+    pageExtensions: ['js', 'jsx', 'mdx']
+  })
+);
