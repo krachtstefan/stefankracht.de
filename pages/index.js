cbd445f784eba8deed86cc175cfed3483dff7874
Stefan Kracht <stefankracht@gmail.com>
2019-03-16 12:40:12 +0100

    add key to each blog item

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ import Link from 'next/link';
 export default () => (
   <Layout>
     {allPosts.map(post => (
-      <>
+      <div key={post.url}>
         <Link as={`/p/${post.url}`} href={`/post?url=${post.url}`}>
           <a>
             <h1>{post.title}</h1>
           </a>
         </Link>
         <post.Component />
-      </>
+      </div>
     ))}
   </Layout>
 );
