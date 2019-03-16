8e352e8fec53deb59b0eed41debb26639a75d43e
Stefan Kracht <stefankracht@gmail.com>
2019-03-16 09:53:49 +0100

    create a static post page

---
+++ pages/post.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import Layout from '../components/layout';
+
+class Index extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>post</h1>
+      </Layout>
+    );
+  }
+}
+export default Index;
