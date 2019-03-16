9b28437ff7d908c53705e5f8576525eedb83c69f
Stefan Kracht <stefankracht@gmail.com>
2019-03-16 10:11:53 +0100

    load the desired blog post on post page

--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,22 @@
 import { withRouter } from 'next/router';
+import Error from 'next/error';
+
+import { findPostbyUrl } from '../lib/blog';
 import Layout from '../components/layout';
 import Link from 'next/link';
 
-export default withRouter(props => (
-  <Layout>
-    <h1>{props.router.query.title}</h1>
-    <Link href={`/`}>
-      <a>back</a>
-    </Link>
-  </Layout>
-));
+export default withRouter(props => {
+  let post = findPostbyUrl(props.router.query.url);
+  if (!post) return <Error statusCode={404} />;
+  return (
+    <Layout>
+      <p>
+        <Link href={`/`}>
+          <a>back</a>
+        </Link>
+      </p>
+      <h1>{post.title}</h1>
+      <post.Component />
+    </Layout>
+  );
+});
