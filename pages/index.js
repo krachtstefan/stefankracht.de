7b1de66cf0b868459acbdb1c7bba8c30b5eeafa3
Stefan Kracht <stefankracht@gmail.com>
2019-03-16 10:18:26 +0100

    use link as for clean url

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default () => (
   <Layout>
     {allPosts.map(post => (
       <>
-        <Link href={`/post?url=${post.url}`}>
+        <Link as={`${post.url}`} href={`/post?url=${post.url}`}>
           <a>
             <h1>{post.title}</h1>
           </a>
