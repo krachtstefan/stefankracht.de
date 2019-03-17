1a09fa5f941cf5589a99dd724b98ffa48da3aa8e
Stefan Kracht <stefankracht@gmail.com>
2019-03-17 07:24:54 +0100

    move logic from the blog list to the index page

--- components/blog/list.js
+++ components/blog/list.js
@@ -1,30 +1,21 @@
 import { withRouter } from 'next/router';
-import { getPostsList, allPosts } from '../../lib/blog';
-import Pagination from './pagination';
+import { getPostsList } from '../../lib/blog';
 import Link from 'next/link';
-import Error from 'next/error';
 
 export default withRouter(props => {
-  let { itemsPerPage } = props;
-  let postCount = allPosts.length;
-  let lastPage = Math.ceil(postCount / itemsPerPage);
-  let page =
-    props.router.query.page && lastPage ? parseInt(props.router.query.page) : 1;
-  let offset = (page - 1) * itemsPerPage;
-  if (page > lastPage) return <Error statusCode={404} />;
+  let { limit, offset } = props;
   return (
     <>
-      {getPostsList({ limit: itemsPerPage, offset }).map(post => (
+      {getPostsList({ limit, offset }).map(post => (
         <div key={post.url}>
           <Link as={`/p/${post.url}`} href={`/post?url=${post.url}`}>
             <a>
               <h1>{post.title}</h1>
             </a>
           </Link>
-          <post.Component />
+          {/* <post.Component /> */}
         </div>
       ))}
-      <Pagination page={page} lastPage={lastPage} />
     </>
   );
 });
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import Layout from '../components/layout';
 import { withRouter } from 'next/router';
 import Bloglist from '../components/blog/list';
+import { allPosts } from '../lib/blog';
+// import Blogpagination from './components/blog/pagination';
 import { config } from '../config';
 
 export default withRouter(props => {
+  let postCount = allPosts.length;
+  let itemsPerPage = config.blog.itemsPerPage;
+  let lastPage = Math.ceil(postCount / itemsPerPage);
+  let page =
+    props.router.query.page && lastPage ? parseInt(props.router.query.page) : 1;
+  let offset = (page - 1) * itemsPerPage;
+  if (page > lastPage) return <Error statusCode={404} />;
+
   return (
     <Layout>
-      <Bloglist itemsPerPage={config.blog.itemsPerPage} />
+      <Bloglist limit={itemsPerPage} offset={offset} />
+      {/* <Blogpagination page={page} lastPage={lastPage} /> */}
     </Layout>
   );
 });
