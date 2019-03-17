6a4a31cbaf81425e50e7c38c31922e9343224df6
Stefan Kracht <stefankracht@gmail.com>
2019-03-17 07:34:07 +0100

    make blog list unaware of the store

--- components/blog/list.js
+++ components/blog/list.js
@@ -1,21 +1,17 @@
 import { withRouter } from 'next/router';
-import { getPostsList } from '../../lib/blog';
 import Link from 'next/link';
 
-export default withRouter(props => {
-  let { limit, offset } = props;
-  return (
-    <>
-      {getPostsList({ limit, offset }).map(post => (
-        <div key={post.url}>
-          <Link as={`/p/${post.url}`} href={`/post?url=${post.url}`}>
-            <a>
-              <h1>{post.title}</h1>
-            </a>
-          </Link>
-          {/* <post.Component /> */}
-        </div>
-      ))}
-    </>
-  );
-});
+export default withRouter(props => (
+  <>
+    {props.posts.map(post => (
+      <div key={post.url}>
+        <Link as={`/p/${post.url}`} href={`/post?url=${post.url}`}>
+          <a>
+            <h1>{post.title}</h1>
+          </a>
+        </Link>
+        {/* <post.Component /> */}
+      </div>
+    ))}
+  </>
+));
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 import Layout from '../components/layout';
 import { withRouter } from 'next/router';
 import Bloglist from '../components/blog/list';
-import { allPosts } from '../lib/blog';
+import { allPosts, getPostsList } from '../lib/blog';
 import Blogpagination from '../components/blog/pagination';
 import { config } from '../config';
 import Error from 'next/error';
 
 export default withRouter(props => {
+  let { query } = props.router;
   let postCount = allPosts.length;
   let itemsPerPage = config.blog.itemsPerPage;
   let lastPage = Math.ceil(postCount / itemsPerPage);
-  let page =
-    props.router.query.page && lastPage ? parseInt(props.router.query.page) : 1;
+  let page = query.page && lastPage ? parseInt(query.page) : 1;
   let offset = (page - 1) * itemsPerPage;
   if (page > lastPage) return <Error statusCode={404} />;
   return (
     <Layout>
-      <Bloglist limit={itemsPerPage} offset={offset} />
+      <Bloglist posts={getPostsList({ limit: itemsPerPage, offset })} />
       <Blogpagination page={page} lastPage={lastPage} />
     </Layout>
   );
