14d7763554bd7a19d55bd990587fc03469544253
Stefan Kracht <stefankracht@gmail.com>
2019-03-27 09:33:41 +0100

    add category names and routing property

--- config/index.js
+++ config/index.js
@@ -1,5 +1,13 @@
 export const config = {
+  routing: {},
   blog: {
-    itemsPerPage: 3
+    itemsPerPage: 3,
+    categories: {
+      raceday: 'Raceday.me',
+      runverter: 'Runverter',
+      stickerlicious: 'Stickerlicious',
+      programming: 'Programming',
+      general: 'General'
+    }
   }
 };
