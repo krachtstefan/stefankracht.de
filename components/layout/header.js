fa758ecdb394c00777f6f66bde4ace840bc7cf0f
Stefan Kracht <stefankracht@gmail.com>
2019-03-30 10:02:16 +0100

    display logo

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,16 +1,24 @@
 import Link from 'next/link';
-export default ({ user = {} }) => (
-  <header>
-    <nav>
-      <Link href="/">
-        <a>Blog</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/imprint">
-        <a>imprint</a>
-      </Link>
-    </nav>
-  </header>
-);
+import React from 'react';
+import Logo from './../../assets/images/logo.svg';
+
+const Header = () => {
+  return (
+    <header>
+      <Logo />
+      {/* <nav>
+        <Link href="/">
+          <a>Blog</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/imprint">
+          <a>imprint</a>
+        </Link>
+      </nav> */}
+    </header>
+  );
+};
+
+export default Header;
