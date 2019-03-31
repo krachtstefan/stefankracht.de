280e5a9b875517b99a94d5dc6ccc65611fe32199
Stefan Kracht <stefankracht@gmail.com>
2019-03-31 12:06:45 +0200

    add class gto blog post

--- components/blog/post.js
+++ components/blog/post.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 
 const Blogpost = props => (
-  <>
+  <div className="blog-post">
     <p>{new Intl.DateTimeFormat('en-US').format(props.post.date)}</p>
     <h1>{props.post.title}</h1>
     <img {...props.post.images.teaser} />
     <props.post.Component />
-  </>
+  </div>
 );
 
 Blogpost.propTypes = {
