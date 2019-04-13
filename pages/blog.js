8546f5ae37b38948931a432eae9b56876a7aa0f4
Stefan Kracht <stefankracht@gmail.com>
2019-04-13 11:07:40 +0200

    make blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,36 @@
 import { withRouter } from 'next/router';
+import { allPosts, getPostsList } from '../lib/blog';
+import { config } from '../config';
+import Error from 'next/error';
+
+import Layout from '../components/layout';
+import Bloglist from '../components/blog/list';
+import Blogpagination from '../components/blog/pagination';
 
 export default withRouter(props => {
-  return <div />;
+  let { query } = props.router;
+  let postCount = allPosts.length;
+  let itemsPerPage = config.blog.itemsPerPage;
+  let lastPage = Math.ceil(postCount / itemsPerPage);
+  let page = query.page && lastPage ? parseInt(query.page) : 1;
+  let offset = (page - 1) * itemsPerPage;
+  if (page > lastPage || [0, 1].includes(parseInt(query.page)))
+    return <Error statusCode={404} />;
+  return (
+    <Layout>
+      <h1>Blog</h1>
+      <Bloglist
+        posts={getPostsList({ limit: itemsPerPage, offset })}
+        posts={getPostsList({ limit: itemsPerPage })}
+        href={url => config.routing.blogPost.nextLink.href(url)}
+        as={url => config.routing.blogPost.nextLink.as(url)}
+      />
+      <Blogpagination
+        page={page}
+        lastPage={lastPage}
+        href={page => config.routing.blogList.nextLink.href(page)}
+        as={page => config.routing.blogList.nextLink.as(page)}
+      />
+    </Layout>
+  );
 });
