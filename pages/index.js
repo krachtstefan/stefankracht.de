5fdfabeb623dd55ac1d9abb17188d91891b81a47
Stefan Kracht <stefankracht@gmail.com>
2019-04-13 11:05:07 +0200

    minify index

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,16 @@
 import { withRouter } from 'next/router';
-import { allPosts, getPostsList } from '../lib/blog';
+import { getPostsList } from '../lib/blog';
 import { config } from '../config';
-import Error from 'next/error';
 import Layout from '../components/layout';
 import ProjectList from '../components/projects/list';
 import Bloglist from '../components/blog/list';
-import Blogpagination from '../components/blog/pagination';
 
 export default withRouter(props => {
-  let itemsPerPage = config.blog.itemsPerPage;
   return (
     <Layout>
       <h1>Blog</h1>
       <Bloglist
-        posts={getPostsList({ limit: itemsPerPage })}
+        posts={getPostsList({ limit: config.blog.itemsPerPage })}
         href={url => config.routing.blogPost.nextLink.href(url)}
         as={url => config.routing.blogPost.nextLink.as(url)}
       />
