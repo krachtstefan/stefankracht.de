c14e65e886fffded19102af739731219f4ee39e2
Stefan Kracht <stefankracht@gmail.com>
2019-04-13 11:02:36 +0200

    minify index

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,23 @@
-import Layout from '../components/layout';
 import { withRouter } from 'next/router';
-import Bloglist from '../components/blog/list';
 import { allPosts, getPostsList } from '../lib/blog';
-import ProjectList from '../components/projects/list';
-import Blogpagination from '../components/blog/pagination';
 import { config } from '../config';
 import Error from 'next/error';
+import Layout from '../components/layout';
+import ProjectList from '../components/projects/list';
+import Bloglist from '../components/blog/list';
+import Blogpagination from '../components/blog/pagination';
 
 export default withRouter(props => {
-  let { query } = props.router;
-  let postCount = allPosts.length;
   let itemsPerPage = config.blog.itemsPerPage;
-  let lastPage = Math.ceil(postCount / itemsPerPage);
-  let page = query.page && lastPage ? parseInt(query.page) : 1;
-  let offset = (page - 1) * itemsPerPage;
-  if (page > lastPage || [0, 1].includes(parseInt(query.page)))
-    return <Error statusCode={404} />;
   return (
     <Layout>
       <h1>Blog</h1>
       <Bloglist
-        posts={getPostsList({ limit: itemsPerPage, offset })}
+        posts={getPostsList({ limit: itemsPerPage })}
         href={url => config.routing.blogPost.nextLink.href(url)}
         as={url => config.routing.blogPost.nextLink.as(url)}
       />
       <a className="more">read more articles</a>
-      {/* <Blogpagination
-        page={page}
-        lastPage={lastPage}
-        href={page => config.routing.blogList.nextLink.href(page)}
-        as={page => config.routing.blogList.nextLink.as(page)}
-      /> */}
       <h1>Projects</h1>
       <ProjectList />
       <a className="more">explore more projects</a>
