5ecd19ee2d545db111284d0e3e74ea283902b462
Stefan Kracht <stefankracht@gmail.com>
2019-04-13 10:12:23 +0200

    add project list

--- components/projects/list.js
+++ components/projects/list.js
@@ -1,5 +1,22 @@
 const ProjectList = () => {
-  return <div>!</div>;
+  return (
+    <div className="project-list">
+      <a>
+        <img
+          src={require('./../../assets/images/projects/raceday-me-logo.svg')}
+          width="90%"
+        />
+        <h1>Personalized race day livetracking for runners</h1>
+      </a>
+      <a>
+        <img
+          src={require('./../../assets/images/projects/runverter-io-logo.svg')}
+          width="90%"
+        />
+        <h1>A minimal and powerful calculator for runners</h1>
+      </a>
+    </div>
+  );
 };
 
 export default ProjectList;
