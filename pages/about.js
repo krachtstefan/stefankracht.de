2ac72470d13b1b8d7cfa568aaa25081a9671be1b
Stefan Kracht <stefankracht@gmail.com>
2019-04-14 07:16:39 +0200

    make about page a functional component

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,9 @@
-import { Component } from 'react';
 import Layout from '../components/layout';
 
-class Index extends Component {
-  render() {
-    return (
-      <Layout>
-        <h1>About</h1>
-      </Layout>
-    );
-  }
-}
-export default Index;
+export default () => {
+  return (
+    <Layout>
+      <h1>About</h1>
+    </Layout>
+  );
+};
