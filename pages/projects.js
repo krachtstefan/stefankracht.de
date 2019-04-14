9e73c735290218a8b18e3676949382a9958d44fa
Stefan Kracht <stefankracht@gmail.com>
2019-04-14 07:25:46 +0200

    make a project list component and render it on the projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,10 @@
 import Layout from '../components/layout';
-
+import ProjectList from '../components/projects/list';
 export default () => {
   return (
     <Layout>
       <h1>Projects</h1>
+      <ProjectList />
     </Layout>
   );
 };
