6ae7a96fdc985344ac5d9c358f0e855fcfe85534
Stefan Kracht <stefankracht@gmail.com>
2019-04-19 10:01:18 +0200

    add blog date

--- components/blog/post.js
+++ components/blog/post.js
@@ -2,10 +2,12 @@ import PropTypes from 'prop-types';
 
 const Blogpost = props => (
   <div className="blog-post">
-    <h1>{props.post.title}</h1>
-    {/* <p>{new Intl.DateTimeFormat('en-US').format(props.post.date)}</p> */}
+    <h1 className="blog-headline">{props.post.title} </h1>
     <img {...props.post.images.teaser} />
     <props.post.Component />
+    <p className="blog-date">
+      written on {new Intl.DateTimeFormat('en-US').format(props.post.date)}
+    </p>
   </div>
 );
 
