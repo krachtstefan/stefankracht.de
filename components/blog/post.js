2e66f2b7ed0fe223bb5ecb3fe399b40fac11e916
Stefan Kracht <stefankracht@gmail.com>
2019-04-19 09:26:27 +0200

    optimize strart of blog article

--- components/blog/post.js
+++ components/blog/post.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 
 const Blogpost = props => (
   <div className="blog-post">
-    {/* <p>{new Intl.DateTimeFormat('en-US').format(props.post.date)}</p> */}
-    <img className="teaser" {...props.post.images.teaser} />
     <h1>{props.post.title}</h1>
+    {/* <p>{new Intl.DateTimeFormat('en-US').format(props.post.date)}</p> */}
+    <img {...props.post.images.teaser} />
     <props.post.Component />
   </div>
 );
