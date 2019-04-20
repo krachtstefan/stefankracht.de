21c2f17c1c2068c9ee89232d9df10664834be47e
Stefan Kracht <stefankracht@gmail.com>
2019-04-20 11:22:36 +0200

    add footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,34 +1,42 @@
 export default () => (
   <footer>
-    You can follow me on
-    <a
-      href="https://twitter.com/stefan_kracht"
-      target="_blank"
-      className="twitter"
-    >
-      Twitter
-    </a>{' '}
-    <a
-      href="https://www.strava.com/athletes/3952530"
-      target="_blank"
-      className="strava"
-    >
-      Strava
-    </a>{' '}
-    <a
-      href="https://www.instagram.com/stefan_kracht/"
-      target="_blank"
-      className="instagram"
-    >
-      Instagram
-    </a>{' '}
-    <a
-      href="https://github.com/krachtstefan"
-      target="_blank"
-      className="github"
-    >
-      Github
-    </a>{' '}
-    - Imprint
+    <p>
+      You can find me on{' '}
+      <a
+        href="https://twitter.com/stefan_kracht"
+        target="_blank"
+        className="twitter"
+      >
+        Twitter
+      </a>
+      ,{' '}
+      <a
+        href="https://www.strava.com/athletes/3952530"
+        target="_blank"
+        className="strava"
+      >
+        Strava
+      </a>
+      ,{' '}
+      <a
+        href="https://www.instagram.com/stefan_kracht/"
+        target="_blank"
+        className="instagram"
+      >
+        Instagram
+      </a>{' '}
+      and{' '}
+      <a
+        href="https://github.com/krachtstefan"
+        target="_blank"
+        className="github"
+      >
+        Github
+      </a>
+    </p>
+    <img
+      id="me"
+      src={require('./../../assets/images/stefan-kracht-pixelart.png')}
+    />
   </footer>
 );
