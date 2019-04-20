ac15d99d8d4731c20d5019551ca63c332c09cd92
Stefan Kracht <stefankracht@gmail.com>
2019-04-20 11:36:39 +0200

    set google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,26 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const isProduction = process.env.NODE_ENV === 'production';
+    const initialProps = await Document.getInitialProps(ctx);
+    console.log('isProduction? ' + isProduction);
+    return { ...initialProps, isProduction };
+  }
+
+  setGoogleTags() {
+    return {
+      __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-748711-5');
+      `
+    };
+  }
+
   render() {
+    const { isProduction } = this.props;
     return (
       <html lang="en">
         <Head>
@@ -10,6 +29,16 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {isProduction && (
+            <Fragment>
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=UA-748711-5"
+              />
+              {/* We call the function above to inject the contents of the script tag */}
+              <script dangerouslySetInnerHTML={this.setGoogleTags()} />
+            </Fragment>
+          )}
         </body>
       </html>
     );
