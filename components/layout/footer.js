6fe7d82c2e27d0299123884861f337212c1478a9
Stefan Kracht <stefankracht@gmail.com>
2019-04-20 10:54:45 +0200

    minimal footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,1 +1,34 @@
-export default () => <footer />;
+export default () => (
+  <footer>
+    You can follow me on
+    <a
+      href="https://twitter.com/stefan_kracht"
+      target="_blank"
+      className="twitter"
+    >
+      Twitter
+    </a>{' '}
+    <a
+      href="https://www.strava.com/athletes/3952530"
+      target="_blank"
+      className="strava"
+    >
+      Strava
+    </a>{' '}
+    <a
+      href="https://www.instagram.com/stefan_kracht/"
+      target="_blank"
+      className="instagram"
+    >
+      Instagram
+    </a>{' '}
+    <a
+      href="https://github.com/krachtstefan"
+      target="_blank"
+      className="github"
+    >
+      Github
+    </a>{' '}
+    - Imprint
+  </footer>
+);
