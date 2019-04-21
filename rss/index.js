53cdc3f904a366d6765a919fac5d7f9dc6efe508
Stefan Kracht <stefankracht@gmail.com>
2019-04-21 10:53:42 +0200

    finish rss

--- rss/index.js
+++ rss/index.js
@@ -1,13 +1,9 @@
+const baseUrl = 'https://stefankracht.de';
 const mdx = require('@mdx-js/mdx');
 const babel = require('@babel/core');
 const fs = require('fs');
-// const path = require('path');
 const RSS = require('rss');
 const blogFolder = './blog/';
-let posts = (fs.readdirSync(`${blogFolder}`) || [])
-  .filter(folderNames => folderNames[0] !== '.') // remove hidden folders
-  .map(postFolder => readPostMetadata(`./blog/${postFolder}/index.mdx`));
-// .map(postFolder => require(`../blog/${postFolder}/index.mdx`));
 
 const requireFromStringSync = (src, filename) => {
   const Module = module.constructor;
@@ -43,47 +39,33 @@ const requireMDXFileSync = path => {
 const readPostMetadata = postPath => {
   const mod = requireMDXFileSync(postPath);
   const { meta } = mod;
-  return {
-    filePath: postPath
-    // urlPath: postPath
-    //   .replace(/\\/, '/')
-    //   .replace(/^pages/, '')
-    //   .replace(/\.mdx?$/, ''),
-    // title: meta.title || path.basename(postPath),
-    // publishDate: new Date(meta.publishDate)
-  };
+  return meta;
 };
-
-// function generateRSS(posts) {
-//   const siteUrl = 'https://nextjs-mdx-blog-example.now.sh';
-//   const feed = new RSS({
-//     title: 'My blog',
-//     site_url: siteUrl
-//   });
-//   posts.map(post => {
-//     feed.item({
-//       title: post.title,
-//       guid: post.urlPath,
-//       url: siteUrl + post.urlPath,
-//       date: post.publishDate
-//     });
-//   });
-//   return feed.xml({ indent: true });
-// }
 
 module.exports = (req, res) => {
   var feed = new RSS({
     title: 'Stefan Kracht Blog',
     description:
       'A mixed collection of articles about development, running and new releases of my current side projects.',
-    feed_url: 'https://stefankracht.de/rss',
-    site_url: 'https://stefankracht.de',
+    feed_url: `${baseUrl}/rss.xml`,
+    site_url: baseUrl,
     language: 'en',
     ttl: '60'
   });
-  console.log(posts);
+
+  let posts = (fs.readdirSync(`${blogFolder}`) || [])
+    .filter(folderNames => folderNames[0] !== '.') // remove hidden folders
+    .map(postFolder => readPostMetadata(`./blog/${postFolder}/index.mdx`));
+  posts.map(post => {
+    feed.item({
+      title: post.title,
+      url: `${baseUrl}/p/${post.url}`,
+      description: post.description,
+      date: post.date
+    });
+  });
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/rss+xml; charset=UTF-8');
-  res.end(feed.xml());
+  res.end(feed.xml({ indent: true }));
 };
