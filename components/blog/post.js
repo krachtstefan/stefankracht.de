00d8d52fd791ff3560ed5c00a546a1b6951f36cf
Stefan Kracht <stefankracht@gmail.com>
2019-04-23 06:30:48 +0200

    remove whitespace

--- components/blog/post.js
+++ components/blog/post.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Blogpost = props => (
   <div className="blog-post">
-    <h1 className="blog-headline">{props.post.title} </h1>
+    <h1 className="blog-headline">{props.post.title}</h1>
     <img {...props.post.images.teaser} />
     <props.post.Component />
     <p className="blog-date">
