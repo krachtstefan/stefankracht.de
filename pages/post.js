12a5cd19f428e650f073a2853ccaed1c02ee1da6
Stefan Kracht <stefankracht@gmail.com>
2019-05-01 15:01:05 +0200

    finish buttons below the article

--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,15 @@
 import { withRouter } from 'next/router';
 import Error from 'next/error';
+import Link from 'next/link';
+
+import { config } from './../config';
 import { findPostbyUrl } from '../lib/blog';
 import Layout from '../components/layout';
-import Link from 'next/link';
 import Blogpost from '../components/blog/post';
 
 export default withRouter(props => {
   let post = findPostbyUrl(props.router.query.url);
+  let blogCategory = post.categories[0];
   if (!post) return <Error statusCode={404} />;
   return (
     <Layout
@@ -16,8 +19,17 @@ export default withRouter(props => {
     >
       <Blogpost post={post} />
       <p className="pagination">
-        <Link href={`/`}>
-          <a className="first only">back</a>
+        <Link
+          as={config.routing.blogList.nextLink.as()}
+          href={config.routing.blogList.nextLink.href()}
+        >
+          <a className="first only">read more articles</a>
+        </Link>
+        <Link
+          as={config.routing.blogCategory.nextLink.as(blogCategory)}
+          href={config.routing.blogCategory.nextLink.href(blogCategory)}
+        >
+          <a className="last only">read more from this category</a>
         </Link>
       </p>
     </Layout>
