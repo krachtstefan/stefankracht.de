9dfa1a469d3d76edbe626f1833136c17f49740b1
Stefan Kracht <stefankracht@gmail.com>
2019-05-04 07:18:43 +0200

    implement selector for scrollprogress

--- components/misc/scrollprogress.js
+++ components/misc/scrollprogress.js
@@ -1,19 +1,26 @@
-export default () => {
+import { useCallback } from 'react';
+export default ({ selector = null }) => {
   const root = document.body.style;
 
+  // returns number between 0 and 1, 1 means it has reached the element/end of the document
+  const scrollProgress = useCallback(() => {
+    let element = selector ? document.querySelector(selector) : null;
+    let distanceFromViewportTop = element
+      ? element.getBoundingClientRect().top + window.pageYOffset
+      : document.body.offsetHeight;
+    let distanceFromViewportBottm =
+      distanceFromViewportTop - window.innerHeight;
+    let progress = window.pageYOffset / distanceFromViewportBottm;
+    return progress > 1 ? 1 : progress;
+  });
+
   // Initialize variables
-  root.setProperty(
-    '--scroll',
-    window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-  );
+  root.setProperty('--scroll', scrollProgress());
 
   // Scroll events
   window.addEventListener('scroll', scroll, false);
   function scroll() {
-    root.setProperty(
-      '--scroll',
-      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-    );
+    root.setProperty('--scroll', scrollProgress());
   }
 
   return <div className="progress" />;
