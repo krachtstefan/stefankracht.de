6d1a3a5f7f5989df6243820f205c64c547380f10
Stefan Kracht <stefankracht@gmail.com>
2019-05-04 11:55:56 +0200

    add mouse sensitive logo hover

--- components/misc/logoAnimation.js
+++ components/misc/logoAnimation.js
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 
 const LogoAnimation = ({ selector = null }) => {
   const element = document.querySelector(selector);
+
   const mouseMove = useCallback(e => {
     let {
       width: elemenWidth,
       height: elementHeigh
     } = element.getBoundingClientRect();
-    let centerX = 0.5;
-    let centerY = 0.5;
-    let posX = e.offsetX / elemenWidth - centerX;
-    let posY = e.offsetY / elementHeigh - centerY;
-    console.log(posX, posY);
+    let posX = (e.offsetX / elemenWidth - 0.5) / 0.5; // between -1 and 1
+    let posY = (e.offsetY / elementHeigh - 0.5) / 0.5; // between -1 and 1
+    element.style.setProperty('--logoRotateX', `${posX * -35}deg`);
+    element.style.setProperty('--logoRotateY', `${posY * 35}deg`);
+    element.style.setProperty('--innerMoveX', `${posX * -10}px`);
+    element.style.setProperty('--innerMoveY', `${posY * -10}px`);
   });
 
   document.querySelector('#logo').addEventListener('mousemove', mouseMove);
