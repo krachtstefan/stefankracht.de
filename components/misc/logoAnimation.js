65c9553b2e53013ef6d2edb3a8faf69e0fdab6f1
Stefan Kracht <stefankracht@gmail.com>
2019-05-04 14:12:24 +0200

    change log animation direction

--- components/misc/logoAnimation.js
+++ components/misc/logoAnimation.js
@@ -11,10 +11,10 @@ const LogoAnimation = ({ selector = null }) => {
     } = element.getBoundingClientRect();
     let posX = (e.offsetX / elemenWidth - 0.5) / 0.5; // between -1 and 1
     let posY = (e.offsetY / elementHeigh - 0.5) / 0.5; // between -1 and 1
-    element.style.setProperty('--logoRotateX', `${posX * -35}deg`);
-    element.style.setProperty('--logoRotateY', `${posY * 35}deg`);
-    element.style.setProperty('--innerMoveX', `${posX * -10}px`);
-    element.style.setProperty('--innerMoveY', `${posY * -10}px`);
+    element.style.setProperty('--logoRotateX', `${posX * 35}deg`);
+    element.style.setProperty('--logoRotateY', `${posY * -35}deg`);
+    element.style.setProperty('--innerMoveX', `${posX * 10}px`);
+    element.style.setProperty('--innerMoveY', `${posY * 10}px`);
   });
 
   document.querySelector('#logo').addEventListener('mousemove', mouseMove);
