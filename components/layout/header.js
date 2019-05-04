e81adfb7c2bfd391141f655e3dafbc8cfdc2bc52
Stefan Kracht <stefankracht@gmail.com>
2019-05-04 08:59:28 +0200

    make simple mouse tracking abouve the logo

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,39 +1,56 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from './../../assets/images/logo.svg';
 
 import ActiveLink from './../../components/misc/activeLink';
 import { config } from '../../config';
 
-const Header = () => {
-  return (
-    <header>
-      <Link href="/">
-        <a id="logo">
-          <Logo />
-        </a>
-      </Link>
-      <nav role="navigation" aria-label="Main navigation">
-        <ActiveLink
-          activeClassName="active"
-          href={config.routing.blogList.nextLink.href(0)}
-          as={config.routing.blogList.nextLink.as(0)}
-          highlightOn={router =>
-            router.pathname === config.routing.blogList.nextLink.href(0) ||
-            router.pathname === '/post'
-          }
-        >
-          <a>Blog</a>
-        </ActiveLink>
-        <ActiveLink activeClassName="active" href="/projects">
-          <a>Projects</a>
-        </ActiveLink>
-        <ActiveLink activeClassName="active" href="/about">
-          <a>About</a>
-        </ActiveLink>
-      </nav>
-    </header>
-  );
-};
+export default class Header extends Component {
+  componentDidMount() {
+    document
+      .querySelector('#logo')
+      .addEventListener('mousemove', this.mouseMove);
+  }
 
-export default Header;
+  mouseMove = e => {
+    let { width: elemenWidth, height: elementHeigh } = document
+      .querySelector('#logo')
+      .getBoundingClientRect();
+    let centerX = 0.5;
+    let centerY = 0.5;
+    let posX = e.offsetX / elemenWidth - centerX;
+    let posY = e.offsetY / elementHeigh - centerY;
+    console.log(posX, posY);
+  };
+
+  render() {
+    return (
+      <header>
+        <Link href="/">
+          <a id="logo">
+            <Logo />
+          </a>
+        </Link>
+        <nav role="navigation" aria-label="Main navigation">
+          <ActiveLink
+            activeClassName="active"
+            href={config.routing.blogList.nextLink.href(0)}
+            as={config.routing.blogList.nextLink.as(0)}
+            highlightOn={router =>
+              router.pathname === config.routing.blogList.nextLink.href(0) ||
+              router.pathname === '/post'
+            }
+          >
+            <a>Blog</a>
+          </ActiveLink>
+          <ActiveLink activeClassName="active" href="/projects">
+            <a>Projects</a>
+          </ActiveLink>
+          <ActiveLink activeClassName="active" href="/about">
+            <a>About</a>
+          </ActiveLink>
+        </nav>
+      </header>
+    );
+  }
+}
