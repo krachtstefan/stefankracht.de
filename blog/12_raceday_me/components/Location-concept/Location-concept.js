6242327dff7f0f56f5dcd2c662202533a2c86a51
Stefan Kracht <stefankracht@gmail.com>
2019-05-15 15:28:29 +0200

    basic svg example

--- blog/12_raceday_me/components/Location-concept/Location-concept.js
+++ blog/12_raceday_me/components/Location-concept/Location-concept.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import SVG from 'svg.js';
+
 let LocationConcept = () => {
-  console.log(window);
-  return <p>LocationConcept</p>;
+  useEffect(() => {
+    var draw = SVG('svg').size(300, 130);
+    var rect = draw
+      .rect(100, 100)
+      .fill('#f06')
+      .move(20, 20);
+  }, []);
+
+  return <div id="svg" />;
 };
 
 export default LocationConcept;
