5c222664f9b87fe4b234d762feddadc5a43cf406
Stefan Kracht <stefankracht@gmail.com>
2019-05-16 07:08:40 +0200

    update LocationConcept wrapper file

--- blog/12_raceday_me/components/Location-concept/index.js
+++ blog/12_raceday_me/components/Location-concept/index.js
@@ -1,9 +1,13 @@
+// this wrapper file is needed to make make use of dynamic imports inside a mdx file
+// otherwise the rss creation script would throw an error
 import dynamic from 'next/dynamic';
 
-const HelloBundle = dynamic(() => import('./Location-concept'), { ssr: false });
+const LocationConcept = dynamic(() => import('./Location-concept'), {
+  ssr: false
+});
 
-let Test = () => {
-  return <HelloBundle />;
+let LocationConceptWrapper = () => {
+  return <LocationConcept />;
 };
 
-export default Test;
+export default LocationConceptWrapper;
