ddc38674bc2ddf8b8807180fdbdfe23c954556f6
Stefan Kracht <stefankracht@gmail.com>
2019-05-17 20:30:10 +0200

    don't use svg.js

--- blog/12_raceday_me/components/Location-concept/Location-concept.js
+++ blog/12_raceday_me/components/Location-concept/Location-concept.js
@@ -1,81 +1,59 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import SVG from 'svg.js';
 
 const Wrapper = styled.div`
-  background: grey;
+  border: 1px solid pink;
+  paddin: 20px;
 `;
 
 let LocationConceptTest = () => {
-  let canvas,
-    track,
-    trackLength,
-    minAccuracy = 3,
-    maxAccuracy = 20,
-    defaultAccuracy = 16,
+  let minAccuracy = 2,
+    maxAccuracy = 10,
+    defaultAccuracy = 5,
     [accuracy, setAccuracy] = useState(defaultAccuracy),
-    ballRefs = Array.from({ length: maxAccuracy });
-
-  const drawCircles = items => {
-    if (canvas) {
-      ballRefs.forEach((ball, index) => {
-        if (!ball) {
-          console.log('init ' + index);
-          ball = canvas.circle(10).fill('#00ff00');
-          ballRefs[index] = ball;
-        } else {
-          console.log('already there ' + index);
-        }
-
-        let visible = index <= items;
-        let center = visible
-          ? track.pointAt((track.length() / items - 1) * index)
-          : {
-              x: Math.floor(Math.random() * 100),
-              y: Math.floor(Math.random() * 100)
-            };
-
-        if (visible) {
-          ball.fill('#0000ff');
-        } else {
-          ball.fill('#00ff00');
-        }
-        ball.cx(center.x).cy(center.y);
-      });
-      // Array.from({ length: items + 1 }).forEach((x, i) => {
-      //   var ball = canvas.circle(10).fill('#00ff00');
-      //   let center = track.pointAt((track.length() / items - 1) * i);
-      //   ball.cx(center.x).cy(center.y);
-      // });
-    } else {
-      console.log('no canvas :(');
-    }
-  };
+    [pathLength, setPathLength] = useState(null),
+    path = useRef(null);
 
   useEffect(() => {
-    canvas = SVG('svg')
-      .size('100%', '100')
-      .viewbox(0, 0, 800, 100);
-
-    track = canvas
-      .path(
-        'M-1,1.499h83c0,0,10.5,1,10.5,11.25s-10.25,12.25-10.25,12.25h-43.5c0,0-10.75,0.75-11.75,11.5s9.75,12.25,9.75,12.25H343.5'
-      )
-      .fill('none')
-      .stroke({ width: 3, color: '#ff0000' });
-
-    drawCircles(defaultAccuracy);
-  }, []);
+    setPathLength(path.current.getTotalLength());
+  }, [useEffect]);
 
   const onSliderChange = (e, x) => {
     setAccuracy(e.target.value);
-    drawCircles(e.target.value);
   };
   return (
     <Wrapper>
-      <div id="svg" />
+      <svg width="100%" height="100" viewBox="0 0 800 100">
+        <path
+          ref={path}
+          d="M-1,1.499h83c0,0,10.5,1,10.5,11.25s-10.25,12.25-10.25,12.25h-43.5c0,0-10.75,0.75-11.75,11.5s9.75,12.25,9.75,12.25H343.5"
+          fill="none"
+          stroke="#ff0000"
+          strokeWidth="3"
+        />
+
+        {path.current &&
+          Array.from({ length: maxAccuracy + 1 }).map((item, index) => {
+            let visible = index < accuracy + 1;
+            let center = visible
+              ? path.current.getPointAtLength(
+                  (pathLength / accuracy + 1) * index
+                )
+              : { x: 0, y: 0 };
+            return (
+              <circle
+                key={index}
+                r="5"
+                cx={center.x}
+                cy={center.y}
+                fill="#00ff00"
+              />
+            );
+          })}
+      </svg>
       <input
         type="range"
         min={minAccuracy}
@@ -84,6 +62,8 @@ let LocationConceptTest = () => {
         onChange={onSliderChange}
       />
       {accuracy}
+      <br />
+      pathLength: {pathLength}
     </Wrapper>
   );
 };
