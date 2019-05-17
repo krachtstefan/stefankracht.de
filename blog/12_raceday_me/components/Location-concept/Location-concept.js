6eda18359ba629fe5eaac1a7c4e3c15b89e9fd36
Stefan Kracht <stefankracht@gmail.com>
2019-05-17 13:45:06 +0200

    make simple interface to create dynamic svg

--- blog/12_raceday_me/components/Location-concept/Location-concept.js
+++ blog/12_raceday_me/components/Location-concept/Location-concept.js
@@ -1,29 +1,64 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 import styled from '@emotion/styled';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import SVG from 'svg.js';
 
 const Wrapper = styled.div`
-  background: pink;
+  background: grey;
 `;
 
-let LocationConcept = () => {
+let LocationConceptTest = () => {
+  let canvas,
+    track,
+    trackLength,
+    minAccuracy = 3,
+    maxAccuracy = 20,
+    defaultAccuracy = 16,
+    [accuracy, setAccuracy] = useState(defaultAccuracy);
+
+  const drawCircles = items => {
+    if (canvas) {
+      Array.from({ length: items + 1 }).forEach((x, i) => {
+        var ball = canvas.circle(10).fill('#00ff00');
+        let center = track.pointAt((track.length() / items - 1) * i);
+        ball.cx(center.x).cy(center.y);
+      });
+    }
+  };
+
   useEffect(() => {
-    var draw = SVG('svg').size(300, 130);
-    var rect = draw
-      .rect(100, 100)
-      .fill('#f06')
-      .move(20, 20);
+    canvas = SVG('svg')
+      .size('100%', '100')
+      .viewbox(0, 0, 800, 100);
+
+    track = canvas
+      .path(
+        'M-1,1.499h83c0,0,10.5,1,10.5,11.25s-10.25,12.25-10.25,12.25h-43.5c0,0-10.75,0.75-11.75,11.5s9.75,12.25,9.75,12.25H343.5'
+      )
+      .fill('none')
+      .stroke({ width: 3, color: '#ff0000' });
+
+    drawCircles(defaultAccuracy);
   }, []);
 
-  const color = 'white';
-
+  const onSliderChange = (e, x) => {
+    setAccuracy(e.target.value);
+    drawCircles(e.target.value);
+  };
   return (
     <Wrapper>
-      <div id="svg" />
+      <p id="svg" />
+      <input
+        type="range"
+        min={minAccuracy}
+        max={maxAccuracy}
+        value={accuracy}
+        onChange={onSliderChange}
+      />
+      {accuracy}
     </Wrapper>
   );
 };
 
-export default LocationConcept;
+export default LocationConceptTest;
